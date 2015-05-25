cc68d412ff70082b2af3320e714a7efe1c084b5a
willowcheng <e.wingus@Gmail.com>
2015-05-25 18:28:00 -0400

    Initial Commit

---
+++ Habitizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Habitizer
+//
+//  Created by Liu Cheng on 2015-05-25.
+//  Copyright (c) 2015 Zhiheng Yi & Liu Cheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
