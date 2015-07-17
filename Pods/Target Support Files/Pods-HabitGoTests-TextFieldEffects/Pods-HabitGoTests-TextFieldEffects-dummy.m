3118d99bdf0f2504bf6ef2b766d75f51be1c09e1
willowcheng <e.wingus@Gmail.com>
2015-07-17 19:08:46 -0400

    Change bundle id and product name

---
+++ Pods/Target Support Files/Pods-HabitGoTests-TextFieldEffects/Pods-HabitGoTests-TextFieldEffects-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HabitGoTests_TextFieldEffects : NSObject
+@end
+@implementation PodsDummy_Pods_HabitGoTests_TextFieldEffects
+@end
---
+++ Pods/Target Support Files/Pods-HabitGoTests/Pods-HabitGoTests-environment.h
@@ -0,0 +1,32 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// BubbleTransition
+#define COCOAPODS_POD_AVAILABLE_BubbleTransition
+#define COCOAPODS_VERSION_MAJOR_BubbleTransition 0
+#define COCOAPODS_VERSION_MINOR_BubbleTransition 2
+#define COCOAPODS_VERSION_PATCH_BubbleTransition 0
+
+// Spring
+#define COCOAPODS_POD_AVAILABLE_Spring
+#define COCOAPODS_VERSION_MAJOR_Spring 1
+#define COCOAPODS_VERSION_MINOR_Spring 0
+#define COCOAPODS_VERSION_PATCH_Spring 3
+
+// SwiftyUserDefaults
+#define COCOAPODS_POD_AVAILABLE_SwiftyUserDefaults
+#define COCOAPODS_VERSION_MAJOR_SwiftyUserDefaults 1
+#define COCOAPODS_VERSION_MINOR_SwiftyUserDefaults 3
+#define COCOAPODS_VERSION_PATCH_SwiftyUserDefaults 0
+
+// TextFieldEffects
+#define COCOAPODS_POD_AVAILABLE_TextFieldEffects
+#define COCOAPODS_VERSION_MAJOR_TextFieldEffects 0
+#define COCOAPODS_VERSION_MINOR_TextFieldEffects 4
+#define COCOAPODS_VERSION_PATCH_TextFieldEffects 0
+
